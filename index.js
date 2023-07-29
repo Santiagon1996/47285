1790a2c23a771e459a09817882c7a59ba2d7dba5
Santiagon1996 <santiagoanardelli96@gmail.com>
2023-07-29 12:01:58 -0300

    EntregaN1

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-class ProductManager{
-
-    constructor(){
-        this.producto=[]
+class ProductManager {
+    #productos
+    static idCounter=0 
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.#productos = []
+        this.title=title,
+        this.description=description,
+        this.price=price,
+        this.thumbnail=thumbnail,
+        this.code=code,
+        this.stock=stock
     }
-
-    getProducts(){
-        console.log(this.producto)
-        return this.producto
-    }
-
-    getProductsById(id) {
-        const buscado = this.producto.find((produc) => produc.id == id)
-        if (buscado) {
-            return buscado
-        } else {
-            console.log(`Not Found`)
-            return undefined
-        }
-    }
-
-    #getProductByCode(code){
-        const codeInArray= this.producto.find((produc)=>produc.code==code)
-        if(codeInArray){
-            return true
-        }else{
-            return false
-        }
-    }   
-    #generarId(){
-        let idGenerado=0
-        for (let i = 0; i < this.producto.length; i++) {
-            const produc = this.producto[i];
-            if(produc.id > idGenerado){
-                idGenerado = produc.id
-            }         
-        }
-        return ++idGenerado
-    }
-
     addProduct(title, description, price, thumbnail, code, stock) {
-        stock= stock ?? 1;
-        const productoNuevo = { title, description, price, stock, thumbnail, id:this.#generarId() }
-        this.producto = [...this.producto, productoNuevo]
+
+        if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined) return console.log("Todos los campos son obligatorios")
+
+        if (this.#productos.find(p => p.code === code)) return console.log("Ya existe un producto con este codigo")
+
+        let productoNuevo = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id : ++ProductManager.idCounter
+        }
+        this.#productos = [...this.#productos, productoNuevo]
         return true
+
     }
- 
+    getProducts() {
+        console.log(this.#productos)
+        return this.#productos
+    }
+    getProductById(id) {
+        const encontrado = this.#productos.find((prod) => prod.id === id)
+        if (encontrado == undefined) {
+            return console.error(`Not Found`)
+        }else{ 
+            return console.log(encontrado)
+        }
+
+    }
 }
+const productManager = new ProductManager()
+productManager.addProduct("pelota", "futbol", "15000", "/images/..", "2", 10)
+productManager.addProduct("musculosa", "ropa", "10000", "/images/", "20", 5)
+productManager.addProduct("auriculares", "tecnologia", "50000", "/images/", "4", 7)
+productManager.getProducts()
+productManager.getProductById(1)
+
 
