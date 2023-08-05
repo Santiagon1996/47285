562b1ad095ee6a0ad19d0a53e5f07d61ceb68a4e
Santiagon1996 <santiagoanardelli96@gmail.com>
2023-08-05 17:38:44 -0300

    EntregaNumero2

--- index.js
+++ index.js
@@ -1,53 +1,110 @@
+import {promises as fs } from "fs"
+
+const path = `./productos.json`
+
 class ProductManager {
-    #productos
-    static idCounter=0 
-    constructor(title, description, price, thumbnail, code, stock) {
-        this.#productos = []
-        this.title=title,
-        this.description=description,
-        this.price=price,
-        this.thumbnail=thumbnail,
-        this.code=code,
-        this.stock=stock
+    constructor() {
+        this.products = []
     }
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(product) {
+        if (product.title == undefined || product.description == undefined || product.price == undefined || product.thumbnail == undefined || product.code == undefined || product.stock == undefined) return console.log(`todos los campos son obligatorios`);
 
-        if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined) return console.log("Todos los campos son obligatorios")
+        
+        const products =JSON.parse(await fs.readFile(path, `utf-8`))
+        const encontrado= products.find((p)=> p.id === product.id )
 
-        if (this.#productos.find(p => p.code === code)) return console.log("Ya existe un producto con este codigo")
-
-        let productoNuevo = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-            id : ++ProductManager.idCounter
+        if (encontrado) {
+            console.log(`Producto encontrado`);
+        } else {
+            products.push(product)
+            await fs.writeFile(path, JSON.stringify(products))
         }
-        this.#productos = [...this.#productos, productoNuevo]
-        return true
+    }
 
+    async getProducts() {
+        
+        const prods =JSON.parse(await fs.readFile(path, `utf-8`))
+        console.log(prods);
     }
-    getProducts() {
-        console.log(this.#productos)
-        return this.#productos
-    }
-    getProductById(id) {
-        const encontrado = this.#productos.find((prod) => prod.id === id)
+
+    async getProductsById(id) {
+        
+        const products =JSON.parse(await fs.readFile(path, `utf-8`))
+        const encontrado= products.find((p)=> p.id === id )
+
         if (encontrado == undefined) {
             return console.error(`Not Found`)
-        }else{ 
+        } else {
             return console.log(encontrado)
         }
 
     }
+
+    async updateProducts(id, product){
+        const products =JSON.parse(await fs.readFile(path, `utf-8`))
+        const encontrado= products.findIndex((p)=> p.id === id )
+
+        if (encontrado === -1 ) {
+            return console.error(`Not Found`)
+        } else {
+            products[encontrado].title = product.title
+            products[encontrado].description = product.description
+            products[encontrado].price = product.price
+            products[encontrado].thumbnail = product.thumbnail
+            products[encontrado].code = product.code
+            products[encontrado].stock = product.stock
+            await fs.writeFile(path, JSON.stringify(products))
+        
+        }
+
+    }
+    async deleteProducts(id){
+        const products =JSON.parse(await fs.readFile(path, `utf-8`))
+        const encontrado= products.find((p)=> p.id === id )
+
+        if(encontrado){
+            await fs.writeFile(path, JSON.stringify(products.filter(el=> el.id != id)))
+        } else{
+            console.log(`Not found`);
+        }
+    }
 }
+class Productos {
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+        this.id = Productos.incrimentarId()// mi id es el resultado de lo que devuelva el metodo 
+    }
+    //defino un metodo de clase no de objeto 
+    static incrimentarId() {
+        //Si existe esta propiedad, la aumento en 1 o la creo 
+        if (this.idIncrement) {
+            this.idIncrement++
+        } else {
+            this.idIncrement = 1
+        }
+        return this.idIncrement
+    }
+}
+
+const producto1 = new Productos("pelota", "futbol", 15000, [], "2PP", 10)
+const producto2 = new Productos("musculosa", "ropa", 10000, [], "20MM", 5)
+const producto3 = new Productos("auriculares", "tecnologia", 50000, [], "4AA", 7)
+
 const productManager = new ProductManager()
-productManager.addProduct("pelota", "futbol", "15000", "/images/..", "2", 10)
-productManager.addProduct("musculosa", "ropa", "10000", "/images/", "20", 5)
-productManager.addProduct("auriculares", "tecnologia", "50000", "/images/", "4", 7)
-productManager.getProducts()
-productManager.getProductById(1)
 
+productManager.addProduct(producto1)
+productManager.addProduct(producto2)
+productManager.addProduct(producto3)
 
+// productManager.getProducts()
+
+// productManager.getProductsById(2)
+
+//productManager.updateProducts(1,{title: "mouse", description: "electronica", price: 20000 , thumbnail: [], code:"20TT", stock: 33})
+
+//productManager.deleteProducts(1)
